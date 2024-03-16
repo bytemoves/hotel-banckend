7be490f3750519f5ac78315d6cdff16061888d4c
bytemoves <morganmogean@gmail.com>
2024-03-16 16:38:13 +0300

    devlearn

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/bytemoves/hotel-backend/api"
+	"github.com/bytemoves/hotel-backend/types"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+
+
 func main () {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "Goku",
+		LastName: "Saitama",
+	}
+
+
+	res ,err := coll.InsertOne(ctx,user)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+
 	listenAddr := flag.String("listenAddr",":5000","The listen addr of the api server")
 	flag.Parse()
 

93545c969ff475db78203e2dccc61a3dfc6e6152
bytemoves <morganmogean@gmail.com>
2024-03-17 09:50:28 +0300

    devlearn

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,19 +1,61 @@
 package api
 
 import (
+	"github.com/bytemoves/hotel-backend/db"
 	"github.com/bytemoves/hotel-backend/types"
+	// "github.com/bytemoves/hotel-backend/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-
-func HandleGetUsers(c *fiber.Ctx) error{
-	u := types.User{
-		FirstName: "Goku",
-		LastName: "Saitama",
-	}
-	return c.JSON(u)
+// retrieve
+type UserHandler struct {
+	UserStore db.UserStore
 }
 
-func HandleGetUser(c *fiber.Ctx) error{
-	return c.JSON("Goku")
+//constructor function
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		UserStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
+	var params types.CreateUserParams
+	if err := c.BodyParser(&params); err != nil{
+		return nil
+	}
+
+	user ,err := types.NewUserFromParams(params)
+	if err != nil{
+		return nil
+	}
+
+	InsertedUser ,err := h.UserStore.InsertUser(c.Context(),user)
+	if err != nil{
+		return err
+	}
+	return c.JSON(InsertedUser)
+}
+
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		
+	)
+
+	user, err := h.UserStore.GetUserByID(c.Context(), id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	users ,err := h.UserStore.GetUsers(c.Context())
+	if err != nil{
+		return err
+	}
+	return c.JSON(users)
 }
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	
 	"log"
 
 	"github.com/bytemoves/hotel-backend/api"
-	"github.com/bytemoves/hotel-backend/types"
+	"github.com/bytemoves/hotel-backend/db"
+	// "github.com/bytemoves/hotel-backend/types"
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -21,38 +22,39 @@ const userColl = "users"
 
 
 
+// Create a new fiber instance with custom config
+var config = fiber.Config{
+    
+    ErrorHandler: func(c *fiber.Ctx, err error) error {
+      
+        return c.JSON(map[string]string{"error": err.Error()})
+    },
+}
 func main () {
+
+	
+
+	listenAddr := flag.String("listenAddr",":5000","The listen addr of the api server")
+	flag.Parse()
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx := context.Background()
-	coll := client.Database(dbname).Collection(userColl)
-
-	user := types.User{
-		FirstName: "Goku",
-		LastName: "Saitama",
-	}
+	
+	//handler initialization
 
 
-	res ,err := coll.InsertOne(ctx,user)
-	if err != nil{
-		log.Fatal(err)
-	}
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
-	fmt.Println(res)
-
-	listenAddr := flag.String("listenAddr",":5000","The listen addr of the api server")
-	flag.Parse()
-
-
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	
-	apiv1.Get("/user",api.HandleGetUsers)
-	apiv1.Get("/user:id",api.HandleGetUser)
+
+
+	apiv1.Post("/user",userHandler.HandlePostUser)
+	apiv1.Get("/user",userHandler.HandleGetUser)
+	apiv1.Get("/user:id",userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
